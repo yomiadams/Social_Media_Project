977a673ecbd070fc0c114d7ac628261ef37c92a6
Johnny Bhojwani <johnny_bhojwani@baylor.edu>
2023-04-18 12:21:07 -0500

    updated project

--- FeedApp/admin.py
+++ FeedApp/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile,Comment,Like, Relationship
 
-admin.site.register(Profile)
-admin.site.register(Comment)
-admin.site.register(Like)
-admin.site.register(Relationship)
+
